cd78cd7c9f7035b17e7fea978488f20c68865789
sebastianstricker <nw258@uni-heidelberg.de>
2021-10-03 16:53:51 +0200

    Update cluster.py
    Added missing positional argument for run_algorithm in cluster.py

--- cluster.py
+++ cluster.py
@@ -55,7 +55,7 @@ try:
         LOGGER.info("Running....please wait...")
 
         try:
-            bpm = run_algorithm(well_frame_paths, video_metadata, args)
+            bpm = run_algorithm(well_frame_paths, video_metadata, args, resulting_dict_from_crop)
             LOGGER.info("Reported BPM: " + str(bpm))
 
         except Exception as e:
