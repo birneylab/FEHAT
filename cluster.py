0ce1e653ba2f7b494427da928ec72dc21be6daa6
Sebastian Stricker <nw258@uni-heidelberg.de>
2021-10-31 13:39:38 +0100

    fixed typo throwing error

--- cluster.py
+++ cluster.py
@@ -21,91 +21,94 @@ from medaka_bpm import run_algorithm
 LOGGER = logging.getLogger(__name__)
 
 ################################## STARTUP SETUP ##################################
-# Parse input arguments
-args = setup.parse_arguments()
-
-tmp_dir = os.path.join(args.outdir, 'tmp')
-
-if args.lsf_index[0] == '\\':
-    # this is for fixing a weird behaviour: the lsf_index comes with a "\" as the first character. The "\" is usefull to pass the parameter, but need to be deleted here.
-    args.lsf_index = args.lsf_index[1:]
-
-well_id = 'WE00' + '{:03d}'.format(int(args.lsf_index))
-analysis_id = args.channels + '-' + args.loops + '-' + well_id
-
-# check if there is a croppedRawTiff folder  ( I don´t know how to solve more elegantly, as the 'args = setup.parse_arguments()' above get the indir originally passed as argument, even if we change it later)
-subdir_list = os.listdir(args.indir)
-for n in subdir_list:
-    if 'croppedRAWTiff' in n:
-        args.indir = os.path.join(args.indir, 'croppedRAWTiff/', '')
-
-# Check if video for well id exists before producting data. Also check in CroppedRAWTiff folder.
-if not io_operations.well_video_exists(args.indir, args.channels, args.loops, well_id):
-    sys.exit()
-
-setup.config_logger(tmp_dir, (analysis_id + ".log"), args.debug)
-
-# need to pass as list to generator
-channels = [args.channels]
-loops = [args.loops]
-
-# Run analysis
-results = { 'channel':          [], 
-            'loop':             [],
-            'well':             [], 
-            'heartbeat':        [],
-            'Heart size':       [], # qc_attributes
-            'HROI count':       [],
-            'Stop frame':       [],
-            'Number of peaks':  [],
-            'Prominence':       [],
-            'Height':           [],
-            'Low variance':     []}
-
-# NOTE: added for compatibility, not working at this moment.
-resulting_dict_from_crop = {}
 try:
-    LOGGER.info("##### Analysis #####")
-    bpm = None
-    qc_attributes = {   "Heart size": None, 
-                        "HROI count": None, 
-                        "Stop frame": None, 
-                        "Number of peaks": None,
-                        "Prominence": None,
-                        "Height": None,
-                        "Low variance": None}
+    # Parse input arguments
+    args = setup.parse_arguments()
 
-    for well_frame_paths, video_metadata in io_operations.well_video_generator(args.indir, channels, loops):
-        if (video_metadata['well_id'] != well_id):
-            continue
+    tmp_dir = os.path.join(args.outdir, 'tmp')
 
-        LOGGER.info(
-            "The analyse for each well can take about 1 to a few minutes")
-        LOGGER.info("Running....please wait...")
+    if args.lsf_index[0] == '\\':
+        # this is for fixing a weird behaviour: the lsf_index comes with a "\" as the first character. The "\" is usefull to pass the parameter, but need to be deleted here.
+        args.lsf_index = args.lsf_index[1:]
 
-        try:
-            bpm, qc_attributes = run_algorithm(well_frame_paths, video_metadata, args, resulting_dict_from_crop)
-            LOGGER.info("Reported BPM: " + str(bpm))
+    well_id = 'WE00' + '{:03d}'.format(int(args.lsf_index))
+    analysis_id = args.channels + '-' + args.loops + '-' + well_id
 
-        except Exception as e:
-            LOGGER.exception("Couldn't acquier BPM for well " + str(video_metadata['well_id'])
-                             + " in loop " + str(video_metadata['loop'])
-                             + " with channel " + str(video_metadata['channel']))
+    # check if there is a croppedRawTiff folder  ( I don´t know how to solve more elegantly, as the 'args = setup.parse_arguments()' above get the indir originally passed as argument, even if we change it later)
+    subdir_list = os.listdir(args.indir)
+    for n in subdir_list:
+        if 'croppedRAWTiff' in n:
+            args.indir = os.path.join(args.indir, 'croppedRAWTiff/', '')
 
+    # Check if video for well id exists before producting data. Also check in CroppedRAWTiff folder.
+    if not io_operations.well_video_exists(args.indir, args.channels, args.loops, well_id):
+        sys.exit()
+
+    setup.config_logger(tmp_dir, (analysis_id + ".log"), args.debug)
+
+    # need to pass as list to generator
+    channels = [args.channels]
+    loops = [args.loops]
+
+    # Run analysis
+    results = { 'channel':          [], 
+                'loop':             [],
+                'well':             [], 
+                'heartbeat':        [],
+                'Heart size':       [], # qc_attributes
+                'HROI count':       [],
+                'Stop frame':       [],
+                'Number of peaks':  [],
+                'Prominence':       [],
+                'Height':           [],
+                'Low variance':     []}
+
+    # NOTE: added for compatibility, not working at this moment.
+    resulting_dict_from_crop = {}
+    try:
+        LOGGER.info("##### Analysis #####")
+        bpm = None
+        qc_attributes = {   "Heart size": None, 
+                            "HROI count": None, 
+                            "Stop frame": None, 
+                            "Number of peaks": None,
+                            "Prominence": None,
+                            "Height": None,
+                            "Low variance": None}
+
+        for well_frame_paths, video_metadata in io_operations.well_video_generator(args.indir, channels, loops):
+            if (video_metadata['well_id'] != well_id):
+                continue
+
+            LOGGER.info(
+                "The analyse for each well can take about 1 to a few minutes")
+            LOGGER.info("Running....please wait...")
+
+            try:
+                bpm, qc_attributes = run_algorithm(well_frame_paths, video_metadata, args, resulting_dict_from_crop)
+                LOGGER.info("Reported BPM: " + str(bpm))
+
+            except Exception as e:
+                LOGGER.exception("Couldn't acquier BPM for well " + str(video_metadata['well_id'])
+                                + " in loop " + str(video_metadata['loop'])
+                                + " with channel " + str(video_metadata['channel']))
+
+    except Exception as e:
+        LOGGER.exception("Couldn't finish analysis")
+
+    finally:
+        # write bpm in tmp directory
+        if bpm:
+            bpm = str(bpm)
+        else:
+            bpm = 'NA'
+
+        out_string = "heartbeat:" + bpm
+        for key, value in qc_attributes.items():
+            out_string += (";" + str(key) + ':' + str(value))
+
+        out_file = os.path.join(tmp_dir, (analysis_id + '.txt'))
+        with open(out_file, 'w') as output:
+            output.write(out_string)
 except Exception as e:
-    LOGGER.exception("Couldn't finish analysis")
-
-finally:
-    # write bpm in tmp directory
-    if bpm:
-        bpm = str(bpm)
-    else:
-        bpm = 'NA'
-
-    out_string = "heartbeat:" + bpm
-    for key, value in qc_attributes.items():
-        out_string.append(";" + str(key) + ':' + str(value))
-
-    out_file = os.path.join(tmp_dir, (analysis_id + '.txt'))
-    with open(out_file, 'w') as output:
-        output.write(out_string)
+    LOGGER.exception("In analysis dispatched to a cluster node")
