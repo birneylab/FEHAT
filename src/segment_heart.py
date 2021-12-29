b7d3594289f27fb05865e066f67cb1dbeb1d3ab3
Sebastian Stricker <nw258@uni-heidelberg.de>
2021-12-29 18:30:05 +0100

    rfft

--- src/segment_heart.py
+++ src/segment_heart.py
@@ -1776,6 +1776,7 @@ def fourier_bpm_slowmode(norm_frames, times, empty_frames, frame2frame_sec, args
 
     return bpm_fourier
 
+# WIP: Rework of old method, no savgol(bad idea) and requiring minimum amplitude of peak
 def new_fourier(hroi_pixels, times, out_dir):
 
     minBPM = 15
@@ -1836,6 +1837,7 @@ def new_fourier(hroi_pixels, times, out_dir):
 
     return bpm, nr_peaks, height, prominence
 
+# WIP: trying to remove some error cases with lower harmonic frequency (2-chamber problem)  
 def peak_detection(average_frequencies, freqs, ax):
     bpm         = None
     nr_peaks    = None
@@ -1894,7 +1896,7 @@ def peak_detection(average_frequencies, freqs, ax):
     # Hz to bpm
     return bpm, nr_peaks, height, prominence
 
-# Try  taking the max of max + limit signals with deviation from median
+# WIP: Try taking the max of max + limit signals with deviation from median
 def new_fourier_2(hroi_pixels, times, outdir):
 
     minBPM = 15
@@ -1970,6 +1972,8 @@ def new_fourier_2(hroi_pixels, times, outdir):
 
     # return bpm, nr_peaks, height, prominence
 
+# Plots amplitudes for each frequency on x axis, pixels on y axis.
+# 2D plot of frequencies detected in the region.
 def plot_frequencies_2d(amplitudes, bins, outdir):
 
     fig = plt.figure()
@@ -1989,6 +1993,85 @@ def plot_frequencies_2d(amplitudes, bins, outdir):
 
     plt.close()
 
+# WIP: new_fourier(), but using old_fourier_restructured as basis
+def new_fourier_3(hroi_pixels, times, out_dir):
+    highest_freqs = []
+
+    pixel_signals = PixelSignal(hroi_pixels)
+
+    for pixel_signal in pixel_signals:
+
+        # smoothe signal
+        pixel_signal = savgol_filter(pixel_signal, window_length=5, polyorder=3)
+
+        # Subtract any linear trends
+        # Increases classification rate slightly (~0.5-1%)
+        # TODO: Set qc attribute to analyse and compare impact
+        pixel_signal = detrend(pixel_signal)
+
+        # Fast fourier transform
+        fourier = np.fft.rfft(pixel_signal)
+
+        # Power Spectral Density
+        psd = np.abs(fourier) ** 2
+
+        N = pixel_signal.size
+        timestep = np.mean(np.diff(times))
+        freqs = np.fft.rfftfreq(N, d=timestep)
+
+        # one-sided Fourier spectra
+        psd = psd[freqs > 0]
+        freqs = freqs[freqs > 0]
+
+        # Determine the peak within the heart range
+        heart_indices = np.where(np.logical_and(freqs >= 0.25, freqs <= 5.0))[0]
+
+        # Spectra within heart range
+        heart_psd = psd[heart_indices]
+        heart_freqs = freqs[heart_indices]
+
+        # Index of largest spectrum in heart range
+        index_max = np.argmax(heart_psd)
+        
+        # Corresponding frequency
+        highest_freq = heart_freqs[index_max]
+        highest_freqs.append(highest_freq)
+
+    density = gaussian_kde(highest_freqs)
+    xs = np.linspace(0.25, 5.0, 500)
+    ys = density(xs)
+
+    peaks, peak_attributes = find_peaks(ys, prominence=1.0)
+    
+    # Calculate bpm from most common Fourier peak
+    max_index = np.argmax(ys)
+
+    # let´s find the max peaks, and use it in case we have two peaks and user has input an average as argument to filter peaks
+    max_x = xs[max_index]
+
+    # Set attributes any peak was found
+    if len(peaks) == 1:
+        LOGGER.info("Found 1 peak")
+        bpm = max_x * 60
+
+    elif len(peaks) > 1:
+        # Peak with maximum prominence
+        max_prominence_idx = np.argmax(peak_attributes['prominences'])
+
+        # Map idx in peak list to idx for x and y values in the frequency-density space.
+        max_prominence_idx = peaks[max_prominence_idx]
+
+        bpm = xs[max_prominence_idx] * 60
+    else:
+        bpm = None
+
+    # Round bpm
+    if bpm is not None:
+        bpm = np.around(bpm, decimals=2)
+
+    return bpm
+
+# Current method, just cleaner and simpler
 def old_fourier_restructured(hroi_pixels, times, out_dir):
     highest_freqs = []
 
@@ -2000,7 +2083,8 @@ def old_fourier_restructured(hroi_pixels, times, out_dir):
         pixel_signal = savgol_filter(pixel_signal, window_length=5, polyorder=3)
 
         # Subtract any linear trends
-        # TODO: Set qc attribute to check effectiveness
+        # Increases classification rate slightly (~0.5-1%)
+        # TODO: Set qc attribute to analyse and compare impact
         pixel_signal = detrend(pixel_signal)
 
         # Fast fourier transform
@@ -2011,10 +2095,6 @@ def old_fourier_restructured(hroi_pixels, times, out_dir):
         N = pixel_signal.size
         timestep = np.mean(np.diff(times))
         freqs = np.fft.fftfreq(N, d=timestep)
-
-        # one-sided Fourier spectra
-        psd = psd[freqs > 0]
-        freqs = freqs[freqs > 0]
 
         # Determine the peak within the heart range
         heart_indices = np.where(np.logical_and(freqs >= 0.25, freqs <= 5.0))[0]
@@ -2343,7 +2423,8 @@ def run(video, args, video_metadata):
     # Run normally, Fourier in segemented area
     if not args['slowmode']:
         #bpm, clear_signal_ratio, chosen_freq_dominance = new_fourier_2(hroi_pixels, times, out_dir)
-        bpm = old_fourier_restructured(hroi_pixels, times, out_dir)
+        #bpm = old_fourier_restructured(hroi_pixels, times, out_dir)
+        bpm = new_fourier_3(hroi_pixels, times, out_dir)
         #bpm, nr_peaks, height, prominence = new_fourier(hroi_pixels, times, out_dir)
         #bpm, nr_peaks, prominence, height, has_low_variance  = fourier_bpm(hroi_pixels, times, empty_frames, frame2frame, args, out_dir)
 
